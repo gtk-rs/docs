0892cd8d82acec1e2448319eecba94425ed41441
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-03-05 22:27:42 +0100

    Docs for the new release

---
+++ implementors/core/iter/traits/trait.TrustedLen.js
@@ -0,0 +1,16 @@
+(function() {var implementors = {};
+implementors["cairo"] = ["impl&lt;T&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/iter/traits/trait.TrustedLen.html" title="trait core::iter::traits::TrustedLen">TrustedLen</a> for <a class="struct" href="https://doc.rust-lang.org/nightly/collections/vec/struct.IntoIter.html" title="struct collections::vec::IntoIter">IntoIter</a>&lt;T&gt;",];
+implementors["gdk"] = ["impl&lt;T&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/iter/traits/trait.TrustedLen.html" title="trait core::iter::traits::TrustedLen">TrustedLen</a> for <a class="struct" href="https://doc.rust-lang.org/nightly/collections/vec/struct.IntoIter.html" title="struct collections::vec::IntoIter">IntoIter</a>&lt;T&gt;",];
+implementors["gdk_pixbuf"] = ["impl&lt;T&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/iter/traits/trait.TrustedLen.html" title="trait core::iter::traits::TrustedLen">TrustedLen</a> for <a class="struct" href="https://doc.rust-lang.org/nightly/collections/vec/struct.IntoIter.html" title="struct collections::vec::IntoIter">IntoIter</a>&lt;T&gt;",];
+implementors["gio"] = ["impl&lt;T&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/iter/traits/trait.TrustedLen.html" title="trait core::iter::traits::TrustedLen">TrustedLen</a> for <a class="struct" href="https://doc.rust-lang.org/nightly/collections/vec/struct.IntoIter.html" title="struct collections::vec::IntoIter">IntoIter</a>&lt;T&gt;",];
+implementors["glib"] = ["impl&lt;T&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/iter/traits/trait.TrustedLen.html" title="trait core::iter::traits::TrustedLen">TrustedLen</a> for <a class="struct" href="https://doc.rust-lang.org/nightly/collections/vec/struct.IntoIter.html" title="struct collections::vec::IntoIter">IntoIter</a>&lt;T&gt;",];
+implementors["gtk"] = ["impl&lt;T&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/iter/traits/trait.TrustedLen.html" title="trait core::iter::traits::TrustedLen">TrustedLen</a> for <a class="struct" href="https://doc.rust-lang.org/nightly/collections/vec/struct.IntoIter.html" title="struct collections::vec::IntoIter">IntoIter</a>&lt;T&gt;",];
+implementors["libc"] = ["impl&lt;T&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/iter/traits/trait.TrustedLen.html" title="trait core::iter::traits::TrustedLen">TrustedLen</a> for <a class="struct" href="https://doc.rust-lang.org/nightly/collections/vec/struct.IntoIter.html" title="struct collections::vec::IntoIter">IntoIter</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/glib/variant/trait.ToVariant.js
+++ implementors/glib/variant/trait.ToVariant.js
@@ -1,5 +1,10 @@
 (function() {var implementors = {};
-implementors["glib"] = [];implementors["gio"] = [];implementors["gtk"] = [];
+implementors["cairo"] = [];
+implementors["gdk"] = [];
+implementors["gdk_pixbuf"] = [];
+implementors["gio"] = [];
+implementors["glib"] = [];
+implementors["gtk"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
